b4d448cff0e5b3c4f7d2be7aa40f8f6db20ece20
DaeLee <masterkorea01@naver.com>
2020-06-17 22:41:55 +0900

    DefaultRouter

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register('post', views.PostViewSet)  # 2개 url 생성
+
+'''
+router.urls => URL 패턴 리스트 
+'''
 
 urlpatterns = [
-
+    path('', include(router.urls)),
 ]
